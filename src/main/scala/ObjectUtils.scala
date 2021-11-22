1a6353bde1f74450844f2f10502957bb1647176b
Carlos de Paula <me@carlosedp.com>
2021-11-22 15:18:58 -0300

    Add object utility implicit classes

---
+++ src/main/scala/ObjectUtils.scala
@@ -0,0 +1,38 @@
+package com.carlosedp.scalautils
+
+object ObjectUtils {
+
+  implicit class StringWithPad(s: String) {
+
+    /**
+     * Left-pads a string to a certain length with a certain character.
+     * @param length The length to pad to.
+     * @param padChar The character to pad with.
+     */
+    def padStr(length: Int, padChar: Char): String =
+      s.reverse.padTo(length, padChar).reverse
+
+    /**
+     * Left-pads a string to a certain length with zero (mostly used for binary strings).
+     * @param length The length to pad to.
+     */
+    def padZero(length: Int): String =
+      s.padStr(length, '0')
+  }
+
+  implicit class Long32Bit(n: Long) {
+    /*
+     * Truncstes a long to 32-bit.
+     * @return The 32-bit long.
+     */
+    def to32Bit: Long = n & 0xffffffffL
+  }
+
+  /*
+   * Truncstes a int to 32-bit and converts to long.
+   * @return The 32-bit long.
+   */
+  implicit class Int32Bit(n: Int) {
+    def to32Bit: Long = n.toLong & 0xffffffffL
+  }
+}
---
+++ src/test/scala/ObjectUtilsSpec.scala
@@ -0,0 +1,47 @@
+import com.carlosedp.scalautils.ObjectUtils._
+import org.scalatest._
+
+import flatspec._
+import matchers.should._
+
+class ObjectUtilsSpec extends AnyFlatSpec with Matchers {
+
+  behavior of "ObjectUtils"
+
+  it should "pad binary string with zeros" in {
+    val myBinary = "100"
+    val output   = myBinary.padZero(12)
+    output should be("000000000100")
+  }
+
+  it should "truncate long 1L to 32 bits" in {
+    val myLong = 1L
+    val output = myLong.to32Bit
+    output should be(1L)
+  }
+
+  it should "truncate long 0xffffffff to 32 bits" in {
+    val myLong = 0xffffffffL
+    val output = myLong.to32Bit
+    output should be(0xffffffffL)
+  }
+
+  it should "truncate long bigger than 0xffffffff to 32 bits" in {
+    val myLong = 0xaaffffffffL
+    val output = myLong.to32Bit
+    output should be(0xffffffffL)
+  }
+
+  it should "truncate int 0xffffffff to 32 bits" in {
+    val myInt  = 0xffffffff
+    val output = myInt.to32Bit
+    output should be(0xffffffffL)
+  }
+
+  it should "truncate int bigger than 0xffffffff to 32 bits" in {
+    val myInt  = 0xaaffffffffL
+    val output = myInt.to32Bit
+    output should be(0xffffffffL)
+  }
+
+}
